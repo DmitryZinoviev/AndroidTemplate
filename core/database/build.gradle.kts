e2023c0d52d09f945bf7e6e79b7093f210e7eafe
Dmitry <da451@mail.ru>
2026-04-19 22:51:37 +0300

    apply plugin

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,12 +1,10 @@
 plugins {
     id("androidtemplate.android.library")
+    id("androidtemplate.kotlin.test")
 }
 
 dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
 }
